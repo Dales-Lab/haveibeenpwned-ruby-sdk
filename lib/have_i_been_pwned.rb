f691ec02a41b629551ebebdc78baaf1a06c5cdea
Dale Myszewski II <dale.myszewski@gmail.com>
2019-06-28 11:48:49 -0500

    Code cleanup and additional cov changes

--- lib/have_i_been_pwned.rb
+++ lib/have_i_been_pwned.rb
@@ -9,26 +9,25 @@ module HaveIBeenPwned
         # get a digest of the password
         digest = Digest::SHA1.hexdigest password
         # get the first 5 characters of the hash
-        firstFive = digest[0..4]
+        first_five = digest[0..4]
         # make the API call
-        results = HTTParty.get("https://api.pwnedpasswords.com/range/#{firstFive}")
-        # if we get something back
-        if results.code == 200
-          # split the string based on line breaks into an array
-          resArray = results.split("\n")
-          # interate through the list of hashes
-          resArray.each do |partial_hash|
-            # hashes are formatted hash:count
-            # ex. 0018A45C4D1DEF81644B54AB7F969B88D65:1
-            # return true if we find a match
-            return true if "#{firstFive}#{partial_hash.split(":")[0]}".upcase == digest.upcase
-          end
+        results = HTTParty.get("https://api.pwnedpasswords.com/range/#{first_five}")
+        
+        # guard: if we dont get something back
+        return unless results.code == 200
 
-          # return false if we dont find anything
-          return false
-        else
-          raise 'Troy Hunt has failed us. The API returned something not 200.'
+        # split the string based on line breaks into an array
+        res_array = results.split("\n")
+        # interate through the list of hashes
+        res_array.each do |partial_hash|
+          # hashes are formatted hash:count
+          # ex. 0018A45C4D1DEF81644B54AB7F969B88D65:1
+          # return true if we find a match
+          return true if "#{first_five}#{partial_hash.split(":")[0]}".upcase == digest.upcase
         end
+
+        # return false if we dont find anything
+        return false
       end
     end
   end
